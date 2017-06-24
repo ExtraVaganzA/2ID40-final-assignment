8d039b04aaa4a26e71b238ffe2e9aa5a47e0a61d
ExtraVaganzA <rodgervdheijden@hotmail.com>
2017-06-24 20:14:18 +0200

    (almost) working day&night temperature settings!

--- js/week.js
+++ js/week.js
@@ -18,3 +18,35 @@ $("#welcome-card-week .delete").click(function(){
     $("#welcome-card-week").hide();
 });
 
+$("#day-temp-plus").click(function(){
+    setDayTemperature((parseInt(localStorage.dayTemperature) + 1).toString());
+});
+
+$("#day-temp-minus").click(function(){
+    setDayTemperature((parseInt(localStorage.dayTemperature) - 1).toString());
+});
+
+$("night-temp-plus").click(function(){
+	setNightTemperature((parseInt(localStorage.nightTemperature) + 1).toString());
+});
+
+$("night-temp-minus").click(function(){
+	setNightTemperature((parseInt(localStorage.nightTemperature) - 1).toString());
+});
+
+// Constants
+const dayTempSelector = "#day-temp-card .value3";
+const nightTempSelector = "#night-temp-card .value4";
+
+function update() {    
+    getDayTemperature(dayTempSelector);
+	getNightTemperature(nightTempSelector);
+	
+    $("#day-temp-plus").prop("disabled", (parseInt(localStorage.dayTemperature) >= 30));
+    $("#day-temp-minus").prop("disabled", (parseInt(localStorage.dayTemperature) <= 5));  
+	$("#night-temp-plus").prop("disabled", (parseInt(localStorage.nightTemperature) >= 30));
+    $("#night-temp-minus").prop("disabled", (parseInt(localStorage.nightTemperature) <= 5)); 
+	
+}
+
+setInterval(function() {update();}, 100);
